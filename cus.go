04542bbf1032c7bf8e0423c29feff7c11f163faa
Habib Ridho <habibridho@live.com>
2018-04-30 13:29:49 +0700

    add put method

--- cus.go
+++ cus.go
@@ -36,3 +36,23 @@ func Post(url string, body interface{}) ([]byte, error) {
 
 	return resBody, nil
 }
+
+func Put(url string, body interface{}) ([]byte, error) {
+	bytesBuffer := new(bytes.Buffer)
+	json.NewEncoder(bytesBuffer).Encode(body)
+	request, err := http.NewRequest(http.MethodPut, url, bytesBuffer)
+	request.Header = map[string][]string{
+		"Content-Type": {"application/json"},
+	}
+	response, err := http.Client{}.Do(request)
+	if err != nil {
+		return nil, err
+	}
+
+	resBody, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return resBody, nil
+}
